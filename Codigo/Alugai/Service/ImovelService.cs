98873305315fb6f9c4c498e79df6a164a980b453
jeovanesf <jeovanesf10@gmail.com>
2021-01-15 16:20:18 -0300

    implementação do imovelservice no core e no service #85

---
+++ Codigo/Alugai/Service/ImovelService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core;
+using Core.Services;
+
+namespace Service
+{
+    public class ImovelService : IImovelService
+    {
+        private readonly alugaiContext _context;
+
+        public ImovelService(alugaiContext context)
+        {
+            _context = context;
+        }
+        public int Inserir(Imovel imovel)
+        {
+            _context.Add(imovel);
+            _context.SaveChanges();
+            return imovel.CodigoImovel;
+        }
+        public void Alterar(Imovel imovel)
+        {
+            _context.Update(imovel);
+            _context.SaveChanges();
+        }
+        public void Excluir(int CodigoImovel)
+        {
+            var _imovel = _context.Imovel.Find(CodigoImovel);
+            _context.Imovel.Remove(_imovel);
+            _context.SaveChanges();
+        }
+        private IQueryable<Imovel> GetQuery()
+        {
+            IQueryable<Imovel> tb_imovel = _context.Imovel;
+            var query = from imovel in tb_imovel select imovel;
+            return query;
+        }
+        public Imovel Buscar(int CodigoImovel)
+        {
+            IEnumerable<Imovel> imoveis = GetQuery().Where(imovelModel => imovelModel.CodigoImovel.Equals(CodigoImovel));
+            return imoveis.ElementAtOrDefault(0);
+        }
+        public IEnumerable<Imovel> ObterTodos()
+        {
+            return GetQuery();
+        }
+    }
+}
