540db0fdf12c343ed786ddb6e8024a498fd47245
adrianocirino <adriano.cirino.da.silva@gmail.com>
2021-01-17 23:42:46 -0300

    Criado Mappers para aluguel

---
+++ Codigo/Alugai/AlugaiWEB/Mappers/AluguelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using AlugaiWEB.Models;
+
+namespace AlugaiWEB.Mappers
+{
+    public class AluguelProfile: Profile
+    {
+        public AluguelProfile()
+        {
+            CreateMap<AluguelModel, Aluguel>().ReverseMap();
+        }
+    }
+}
