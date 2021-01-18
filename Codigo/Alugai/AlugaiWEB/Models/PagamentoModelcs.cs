909fe10c449141f7ca66b5556e153aafbaf51c92
Gilton Carvalho <gilton.carvalho.gcs@gmail.com>
2021-01-18 10:09:42 -0300

    Caso de Uso Manter pagamento issue #88

---
+++ Codigo/Alugai/AlugaiWEB/Models/PagamentoModelcs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlugaiWEB.Models
+{
+    public class PagamentoModelcs
+    {
+        [Display(Name = "Código Pagamento")]
+        [Required(ErrorMessage = "Código da pessoa é Obrigatório")]
+        public int CodigoPagamento { get; set; }
+
+        [Display(Name = "DT. Pagamento")]
+        [DataType(DataType.Date, ErrorMessage = "Data válida requerida")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataPagamento { get; set; }
+
+        [Display(Name = "Valor do Aluguel")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public double Valor { get; set; }
+
+        [Display(Name = "DT. Vencimento")]
+        [DataType(DataType.Date, ErrorMessage = "Data válida requerida")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataVencimento { get; set; }
+
+        [Display(Name = "Forma de Pagamento")]
+        [StringLength(25, MinimumLength = 4, ErrorMessage = "O valor deve ter entre 4 e 15 caracteres!!")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string FormaDePagamento { get; set; }
+
+        [Display(Name = "Codigo Aluguel")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public int CodigoAluguel { get; set; }
+    }
+}
---
+++ Codigo/Alugai/Service/PagamentoService.cs
@@ -0,0 +1,58 @@
+﻿using Core;
+using Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Service
+{
+    public class PagamentoService : IPagamentoService
+    {
+        private readonly alugaiContext _context;
+
+        public PagamentoService(alugaiContext context)
+        {
+            _context = context;
+        }
+
+        public int Inserir(Pagamento pagamento)
+        {
+            _context.Add(pagamento);
+            _context.SaveChanges();
+            return pagamento.CodigoPagamento;
+        }
+
+        public void Excluir(int CodigoPagamento)
+        {
+            var _pagamento = _context.Pagamento.Find(CodigoPagamento);
+            _context.Pagamento.Remove(_pagamento);
+            _context.SaveChanges();
+        }
+
+        public void Alterar(Pagamento pagamento)
+        {
+            _context.Update(pagamento);
+            _context.SaveChanges();
+
+        }
+        private IQueryable<Pagamento> GetQuery()
+        {
+            IQueryable<Pagamento> tb_pagamento = _context.Pagamento;
+            var query = from pagamento in tb_pagamento
+                        select pagamento;
+            return query;
+        }
+        public Pagamento Buscar(int CodigoPagamento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pagamento> ObterTodos()
+        {
+            return GetQuery();
+        }
+
+       
+    }
+}
