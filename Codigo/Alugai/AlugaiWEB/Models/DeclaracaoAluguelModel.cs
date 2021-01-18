2cc2a1e14ded2c9abce32027c51f9eb7fb50fe49
jeovanesf <jeovanesf10@gmail.com>
2021-01-18 01:31:50 -0300

    criacao do projeto core, service, viewmodel e mappers referente a esse caso de uso #96

---
+++ Codigo/Alugai/AlugaiWEB/Models/DeclaracaoAluguelModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlugaiWEB.Models
+{
+    public class DeclaracaoAluguelModel
+    {
+        [Display(Name = "Codigo do Aluguel")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public int CodigoAluguel { get; set; }
+        [Display(Name = "Descricao do Aluguel")]
+        [StringLength(250, MinimumLength = 15, ErrorMessage = "O valor deve ter entre 15 e 250 caracteres!!")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Descricao { get; set; }
+        [Display(Name = "Codigo do Pagamento")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public int CodigoStatusPagamento { get; set; }
+    }
+}
---
+++ Codigo/Alugai/Core/Services/IDeclaracaoAluguelService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Services
+{
+    public interface IDeclaracaoAluguelService
+    {
+        int Inserir(Aluguel aluguel);
+        void Alterar(Aluguel aluguel);
+        void Excluir(int CodigoAluguel);
+        Aluguel Buscar(int CodigoAluguel);
+        IEnumerable<Aluguel> ObterTodos();
+    }
+}
