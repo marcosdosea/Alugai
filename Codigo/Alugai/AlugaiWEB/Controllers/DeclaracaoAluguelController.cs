cadebf17de0321dae900fa3f742f0ae2eb423f0f
jeovanesf <jeovanesf10@gmail.com>
2021-01-18 13:28:04 -0300

    resolvendo bugs #96

---
+++ Codigo/Alugai/AlugaiWEB/Controllers/DeclaracaoAluguelController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Core.Services;
+using Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlugaiWEB.Models;
+
+namespace AlugaiWEB.Controllers
+{
+    public class DeclaracaoAluguelController : Controller
+    {
+        IDeclaracaoAluguelService _declaracaoAluguelService;
+        IMapper _mapper;
+
+        public DeclaracaoAluguelController(IDeclaracaoAluguelService declaracaoAluguelService, IMapper mapper)
+        {
+            _declaracaoAluguelService = declaracaoAluguelService;
+            _mapper = mapper;
+        }
+
+        public ActionResult Index()
+        {
+            var listarDeclaracoes = _declaracaoAluguelService.ObterTodos();
+            var listarDeclaracoesModel = _mapper.Map<List<DeclaracaoAluguelModel>>(listarDeclaracoes);
+
+            return View(listarDeclaracoesModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Aluguel declaracao = _declaracaoAluguelService.Buscar(id);
+            DeclaracaoAluguelModel declaracaoAluguelModel = _mapper.Map<DeclaracaoAluguelModel>(declaracao);
+            return View(declaracaoAluguelModel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DeclaracaoAluguelModel declaracao)
+        {
+            if (ModelState.IsValid)
+            {
+                var aluguel = _mapper.Map<Aluguel>(declaracao);
+                _declaracaoAluguelService.Inserir(aluguel);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Aluguel declaracao = _declaracaoAluguelService.Buscar(id);
+            DeclaracaoAluguelModel declaracaoAluguelModel = _mapper.Map<DeclaracaoAluguelModel>(declaracao);
+            return View(declaracaoAluguelModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, DeclaracaoAluguelModel declaracao)
+        {
+            if (ModelState.IsValid)
+            {
+                var aluguel = _mapper.Map<Aluguel>(declaracao);
+                _declaracaoAluguelService.Alterar(aluguel);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Aluguel declaracao = _declaracaoAluguelService.Buscar(id);
+            DeclaracaoAluguelModel declaracaoAluguelModel = _mapper.Map<DeclaracaoAluguelModel>(declaracao);
+            return View(declaracaoAluguelModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, DeclaracaoAluguelModel declaracao)
+        {
+            _declaracaoAluguelService.Excluir(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Codigo/Alugai/Service/DeclaracaoAluguelService.cs
@@ -0,0 +1,56 @@
+﻿using Core;
+using Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Service
+{
+    public class DeclaracaoAluguelService : IDeclaracaoAluguelService
+    {
+        private readonly alugaiContext _context;
+
+        public DeclaracaoAluguelService(alugaiContext context)
+        {
+            _context = context;
+        }
+
+        public void Alterar(Aluguel aluguel)
+        {
+            _context.Update(aluguel);
+            _context.SaveChanges();
+        }
+
+        public void Excluir(int CodigoAluguel)
+        {
+            var _aluguel = _context.Aluguel.Find(CodigoAluguel);
+            _context.Aluguel.Remove(_aluguel);
+            _context.SaveChanges();
+        }
+
+        public int Inserir(Aluguel aluguel)
+        {
+            _context.Add(aluguel);
+            _context.SaveChanges();
+            return aluguel.CodigoAluguel;
+        }
+
+        private IQueryable<Aluguel> GetQuery()
+        {
+            IQueryable<Aluguel> tb_aluguel = _context.Aluguel;
+            var query = from aluguel in tb_aluguel select aluguel;
+            return query;
+        }
+
+        public Aluguel Buscar (int CodigoAlguel)
+        {
+            IEnumerable<Aluguel> alugueis = GetQuery().Where(AluguelModel => AluguelModel.CodigoAluguel.Equals(CodigoAlguel));
+            return alugueis.ElementAtOrDefault(0);
+        }
+        public IEnumerable<Aluguel> ObterTodos()
+        {
+            return GetQuery();
+        }
+    }
+}
