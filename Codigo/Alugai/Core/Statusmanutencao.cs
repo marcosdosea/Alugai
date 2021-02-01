b9ea46b7421bdfcbb942c8f04fbe9ab6c0905b62
jeovanesf <jeovanesf10@gmail.com>
2021-02-01 15:07:33 -0300

    #117

---
+++ Codigo/Alugai/Core/Statusmanutencao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public partial class Statusmanutencao
+    {
+        public Statusmanutencao()
+        {
+            Manuntencao = new HashSet<Manuntencao>();
+        }
+
+        public int CodigoStatusManutencao { get; set; }
+        public string StatusManutencao1 { get; set; }
+
+        public virtual ICollection<Manuntencao> Manuntencao { get; set; }
+    }
+}
