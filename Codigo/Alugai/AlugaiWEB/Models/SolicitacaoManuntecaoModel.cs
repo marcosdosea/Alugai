ba25f429bb86cdde5dea72b371d1e83462ba4868
Maycon <mayconpalmeiras2013@hotmail.com>
2021-02-01 15:27:23 -0300

    ajustes no caso de uso Solicitacao Manuntecao #121

--- Codigo/Alugai/AlugaiWEB/Models/SolicitacaoManuntecaoModel.cs
+++ Codigo/Alugai/AlugaiWEB/Models/SolicitacaoManuntecaoModel.cs
@@ -10,10 +10,10 @@ namespace AlugaiWEB.Models
     {
 
         [Display(Name = "Código")]
-        [Required(ErrorMessage = "Código da manuntenção é Obrigatório")]
+        [Required(ErrorMessage = "Código da manutenção é Obrigatório")]
         public int CodigoManuntencao { get; set; }
 
-        [Display(Name = "Tipo Da Manuntenção")]
+        [Display(Name = "Tipo Da Manutenção")]
         [StringLength(45, MinimumLength = 3, ErrorMessage = "O Tipo deve ter entre 3 e 50 caracteres")]
         [Required(ErrorMessage = "Campo Obrigatório")]
         public string TipoDeManuntencao { get; set; }
@@ -23,14 +23,9 @@ namespace AlugaiWEB.Models
         [Required(ErrorMessage = "Campo Obrigatório")]
         public string Descricao { get; set; }
 
-        [Display(Name = "Valor Da Manuntenção")]
+        [Display(Name = "Valor Da Manutenção")]
         [Required(ErrorMessage = "O valor da manuntenção é Obrigatório")]
         public double Valor { get; set; }
-
-        [Display(Name = "Status Da Manuntenção")]
-        [StringLength(25, MinimumLength = 3, ErrorMessage = "O Status deve ter entre 3 e 25 caracteres")]
-        [Required(ErrorMessage = "Campo Obrigatório")]
-        public string StatusManuntencao { get; set; }
 
         [Display(Name = "Comodo Do Imóvel")]
         [StringLength(45, MinimumLength = 3, ErrorMessage = "A locolização de onde a manuntenção será feita deve ter entre 3 e 45 caracteres")]
@@ -45,6 +40,10 @@ namespace AlugaiWEB.Models
         [Required(ErrorMessage = "Código é Obrigatório")]
         public int CodigoImovel { get; set; }
 
-   
+        [Display(Name = "Status Atual Da Manutenção Do Imovel")]
+        [Required(ErrorMessage = "Código é Obrigatório")]
+        public int StatusManutencao { get; set; }
+
+
     }
 }
