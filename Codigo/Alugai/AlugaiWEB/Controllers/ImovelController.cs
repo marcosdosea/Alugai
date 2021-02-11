1fb8efed56d6a66b92e2846a52c46035f05211ac
Maycon <mayconpalmeiras2013@hotmail.com>
2021-02-11 18:50:41 -0300

    refatoroção de parte da model,core e service #128

--- Codigo/Alugai/AlugaiWEB/Controllers/ImovelController.cs
+++ Codigo/Alugai/AlugaiWEB/Controllers/ImovelController.cs
@@ -1,12 +1,8 @@
 ﻿using AutoMapper;
 using Core.Services;
 using Core;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AlugaiWEB.Models;
 
 namespace AlugaiWEB.Controllers
--- Codigo/Alugai/AlugaiWEB/Controllers/SolicitacaoManutencaoController.cs
+++ Codigo/Alugai/AlugaiWEB/Controllers/SolicitacaoManutencaoController.cs
@@ -2,12 +2,8 @@
 using AutoMapper;
 using Core;
 using Core.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AlugaiWEB.Controllers
 {
