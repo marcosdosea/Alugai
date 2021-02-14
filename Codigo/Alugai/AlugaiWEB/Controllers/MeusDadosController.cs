9fd9161d85db813ad7f9c65839cc07f0be601b34
adrianocirino <adriano.cirino.da.silva@gmail.com>
2021-02-14 00:40:16 -0300

    Criado MeusDados Controller

---
+++ Codigo/Alugai/AlugaiWEB/Controllers/MeusDadosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlugaiWEB.Controllers
+{
+    public class MeusDadosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
