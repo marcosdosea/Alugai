257f12e8f7db32727d886fdb7a3da802fca10092
Gilton Carvalho <gilton.carvalho.gcs@gmail.com>
2021-06-28 18:31:13 -0300

    Criação dos testes de unidade da controladora Anuncio. issue#168

---
+++ Codigo/Alugai/AlugaiWEBTests/Controllers/SolicitacaoManutencaoControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlugaiWEB.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Core.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using AlugaiWEB.Models;
+using Core;
+
+namespace AlugaiWEB.Controllers.Tests
+{
+    [TestClass()]
+    public class SolicitacaoManutencaoControllerTests
+    {
+        
+    }
+}
